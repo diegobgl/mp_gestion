7ebd443e753312c4d970a84065bcb9e269390869
Diego Gajardo <deskodoo@a2group.cl>
2024-09-05 11:15:57 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': "MP Gestion",
+    'summary': "Se agrega la gestión para la contabilidad",
+    'author': 'I+D, Diego Gajardo, Camilo Neira, Diego Morales',
+    'website': 'https://www.holdconet.com',
+    'category': 'account',
+    'version': '17.0.0.0.1',
+    'depends': ['account', 'l10n_latam_invoice_document', 'payroll_payment', 'account_bank_cash_report'],
+    'data': [
+        'security/mp_gestion_group.xml',
+        'views/account_move_views.xml',
+    ],
+    'license': 'Other proprietary',
+}
---
+++ models/account_move_line.py
@@ -0,0 +1,15 @@
+
+from odoo import models, fields
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    invoice_date = fields.Date(related='move_id.invoice_date', store=True)
+    l10n_latam_document_type_id = fields.Many2one(related='move_id.l10n_latam_document_type_id', store=True)
+    journal_id = fields.Many2one(related='move_id.journal_id', store=True)
+    mp_flujo_id = fields.Many2one(related='move_id.mp_flujo_id', store=True)
+    mp_grupo_flujo_ids = fields.Many2many(related='move_id.mp_grupo_flujo_ids', store=True)
+    mp_grupo_flujo_id = fields.Many2one(related='move_id.mp_grupo_flujo_id', store=True)
+    invoice_origin = fields.Char(related='move_id.invoice_origin', store=True)
+    vat = fields.Char(related='move_id.partner_id.vat', store=True)
+    code_account = fields.Char(related='account_id.code', string='Número de Cuenta Contable', store=True)
